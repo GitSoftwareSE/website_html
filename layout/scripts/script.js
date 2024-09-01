aeef0dd1aab9d68c562bb740129dd7d07ccc2cc9
Andres Patino <andres.patinocas@gmail.com>
2024-09-01 11:50:42 -0500

    refactor: Remove unused menu toggle code

--- layout/scripts/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-  const menuToggle = document.getElementById('menu-toggle');
-  const nav = document.querySelector('#mainav ul');
-
-  menuToggle.addEventListener('click', function() {
-      nav.classList.toggle('active');
-  });
-});
